2c086f2b7ce61c24edf41b0333b3f79edb82016c
Jinghao Sun <jinghao.sun@yale.edu>
2020-10-05 12:29:07 -0400

    Add Polish data

--- R/Polish.R
+++ R/Polish.R
@@ -11,14 +11,3 @@
 #' aantallen-gere gistreerde-en-niet-geregistreerde-burgers-uit-moe-landen-die-
 #' in-nederland-verblijven. html (in Dutch) (accessed July 2015).
 "Polish"
-
-#' Capture-Recapture samples for People Who Inject Drugs in Brussels, Belgium.
-#'
-#' Three capture-recapture samples of PWID in Brussels. See (Els Plettinckx, Forrest W. Crawford,
-#' Jérôme Antoine, Gremeaux Lies, and Luk Van Baelen.
-#' How many people injected drugs over the last 12 months in Belgium? Estimates based on a
-#' capture-recapture and multiplier method. Working Paper, 2020.) and (Sun, Jinghao, Luk Van Baelen,
-#' Els Plettinckx, and Forrest W. Crawford. "Partial identification and dependence-robust confidence
-#' intervals for capture-recapture surveys." arXiv preprint arXiv:2008.00127 (2020))
-#' (https://arxiv.org/abs/2008.00127) for details.
-"PWID"
